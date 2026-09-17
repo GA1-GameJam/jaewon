0ce9a496300b7eeb901ad8baf7dd43937649e2da
choi06151 <78068768+choi06151@users.noreply.github.com>
2026-09-17 22:45:20 +0900

    fix: UI오류 수정
    UiScaler를 적용및 수정

---
+++ Assets/02.Scripts/Environment/AudioManager.cs
@@ -0,0 +1,32 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private static AudioManager _instance;
+    public static AudioManager Instance=>_instance;
+    private AudioSource _audioSource;
+
+
+    private void Awake()
+    {
+        if (_instance==null) 
+        {
+            _instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+        
+        
+    public void PlayEffectClip(AudioClip clip)
+    {
+        _audioSource.PlayOneShot(clip);
+    }
+}
